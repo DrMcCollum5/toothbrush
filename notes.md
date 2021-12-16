3a87fe1c1c0cf471276db1b51e6609071b7db2d2
Akeia M <mccollumakeia@outlook.com>
2021-12-16 17:56:50 -0500

    added function user input

---
+++ notes.md
@@ -0,0 +1,30 @@
+# Notes
+
+## Window prompts
+
+1. alert -> alert("Insert message here) - sends a message
+2. Boolean - > confirm("Insert true false question") - 
+3. Input -> prompt("Enter input question here") - for inputting info
+
+
+## Tyoes of variables
+
+var animal = "dog"
+
+var animalArray = ["dog", "cat", "tiger", "lion"]
+
+var animalObject = {
+    "name": "Charlie",
+    "species": "dog"
+}
+
+var animalArrayOfObjects = [
+    {
+        "name": "Charlie",
+        "species": "dog"
+    },
+    {
+        "name": "Bob",
+        "species": "Cat"
+    }
+]
--- script.js
+++ script.js
@@ -1,50 +1,99 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  console.log()
-}
+var numbers = ["0", "1", "2"]
 
 // DOM elements
-const resultEL = document
+//const resultEL = document;
+
+
+
+// Function user input
+
+function userInput() {
+  var passwordLength = prompt(
+    "How long do you want your password, must be between 8 and 128 characters."
+  );
+  console.log(passwordLength);
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert ("Password must between 8 and 128 characters");
+    return;
+  }
+  var passwordLower = confirm ("Do you want your password to have lowercase characters")
+  var passwordUpper = confirm ("Do you want your password to have uppercase characters")
+  var passwordSymbol = confirm ("Do you want your password to have symbol characters")
+  var passwordNumber = confirm ("Do you want your password to have number characters")
+
+  if (passwordLower=== false && passwordUpper === false && passwordSymbol === false && passwordNumber=== false) {
+    alert ("Must choose at least one out of 4 character types");
+    return
+  }
+
+  var userChoices = {
+    passwordLength: passwordLength,
+    passwordLower: passwordLower,
+    passwordUpper: passwordUpper,
+    passwordSymbol: passwordSymbol,
+    passwordNumber: passwordNumber
+  }
+
+  return userChoices;
+
+
+}
 
 
 
 
 // Objects
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
-};
+// const randomFunc = {
+//   lower: getRandomLower,
+//   upper: getRandomUpper,
+//   number: getRandomNumber,
+//   symbol: getRandomSymbol,
+// };
 
+function generatePassword() {
+  var pwChoices = userInput();
+
+  var result = [];
+  var allUserCharacters = [];
+  var mustUseOne = [];
+
+
+  //4 if statements - onbe for each character type
+
+  if(pwChoices.passwordLower) {
+    //concat arrays to have all options
+    // push one random to mustUseOne array
+  }
+
+  
+
+}
 
 // Generator functions- http://www.net-comber.com/charset.html
 
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
- 
- 
- function getRandomUpper() {
+
+function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
- }
+}
 
- function getRandomNumber() {
+function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
- }
- function getRandomSymbol() {
-   const symbol = '!@#$%^&*(){}=<>/,.';
+}
+function getRandomSymbol() {
+  const symbol = "!@#$%^&*(){}=<>/,.";
   return symbol[Math.floor(Math.random() * symbol.length)];
- }
+}
 
- console.log(getRandomNumber());
- console.log(getRandomLower());
- console.log(getRandomUpper());
- console.log(getRandomSymbol());
-
-
+console.log(getRandomNumber());
+console.log(getRandomLower());
+console.log(getRandomUpper());
+console.log(getRandomSymbol());
 
 // Write password to the #password input
 function writePassword() {
@@ -52,7 +101,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
